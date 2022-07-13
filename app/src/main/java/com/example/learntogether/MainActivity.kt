8e847d571047ece9f4a8d0e3c9ef6b0cee05c6cc
Vaibhav Kesharwani <vaibhav.vk2128@gmail.com>
2022-07-13 15:28:38 +0530

    UI complete

--- app/src/main/java/com/example/learntogether/MainActivity.kt
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.learntogether
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.learntogether.ui.theme.LearnTogetherTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    JetpackComposeTutorial(
+                        stringResource(id = R.string.tutorial_head),
+                        stringResource(id = R.string.tutorial_body1),
+                        stringResource(id = R.string.tutorial_body2)
+                    )
                 }
             }
         }
@@ -30,14 +40,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun JetpackComposeTutorial(head: String, body1: String, body2: String){
+    val image = painterResource(id = R.drawable.bg_compose_background)
+    Column{
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(
+            text = head,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+            )
+        Text(
+            text = body1,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = body2,
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     LearnTogetherTheme {
-        Greeting("Android")
+        JetpackComposeTutorial(
+            stringResource(id = R.string.tutorial_head),
+            stringResource(id = R.string.tutorial_body1),
+            stringResource(id = R.string.tutorial_body2)
+        )
     }
 }
